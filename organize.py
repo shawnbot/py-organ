e228f180300c39d07d7e5fd50b2cee704fb157b9
Shawn Allen <shawn@stamen.com>
2013-07-31 14:56:52 -0700

    help

--- organize.py
+++ organize.py
@@ -14,7 +14,7 @@ def main(reader, **kwargs):
     filter_expr = kwargs.get('filter_expr')
     map_expr = kwargs.get('map_expr')
     allow_empty = kwargs.get('allow_empty', False)
-    filename_template = kwargs.get('filename_template', '%.csv')
+    filename_template = kwargs.get('filename', '%.csv')
     input_format = kwargs.get('input_format', 'csv')
     fieldnames = kwargs.get('fieldnames')
 
@@ -63,17 +63,68 @@ if __name__ == '__main__':
     import csv
 
     usage = """
-    %prog [options] [--key <KEY> | --key-expr <EXPR>] <CSV>
-    """.strip()
+    %prog [options] (--key | -k) <KEY> [<CSV>]
+    %prog [options] (--key-expr | -K) <KEY EXPRESSION> [<CSV>]
+
+%prog takes a single CSV filename (or reads CSV from stdin) and runs a
+"key function" on each row to generate a filesystem path to which the row
+should be written. Some examples:
+
+Organize a CSV file with Year, Month and Date columns into nested
+subdirectories of CSV data:
+
+    %prog --key "{Year}/{Month}/{Date}" path/to/dates.csv
+
+Classify geographic statistics, e.g. in a table that contains rows for
+states, cities and zip codes:
+
+    %prog --filter "Region == 'State'"  --key "states/{State}"
+    %prog --filter "Region == 'City'"   --key "states/{State}/cities"
+    %prog --filter "Region == 'Zip'"    --key "states/{State}/cities/{City}/zips"
+
+    """.rstrip()
 
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option('--key',      '-k',   dest='key')
-    parser.add_option('--key-expr', '-K',   dest='key_expr')
-    parser.add_option('--filename', '-f',   dest='filename_template', default='%s.csv')
-    parser.add_option('--dialect',  '-d',   dest='dialect', default='excel')
-    parser.add_option('--filter',   '-F',   dest='filter_expr')
-    parser.add_option('--map',      '-m',   dest='map_expr')
-    parser.add_option('--empty',    '-e',   dest='allow_empty', action='store_true')
+
+    parser.add_option('--key', '-k', dest='key', help="""
+The key template string, with interpolated keys wrapped in {}, e.g. "states/{State}"
+    """.strip())
+
+    parser.add_option('--key-expr', '-K', dest='key_expr', help="""
+Alternatively, you can provide a key expression, which is evaluated as Python
+code with the row's keys as local variables, e.g. "State[0:2].upper()"
+    """.strip())
+
+    parser.add_option('--filename', '-f', dest='filename', default='%s.csv', help="""
+The format of the output filename for each unique key's values. This should be
+an Python formatting string in which %s is replaced with the key, e.g. "%s.txt"
+(default: "%s.csv")
+    """.strip())
+
+    parser.add_option('--dialect', '-d', dest='dialect', default='excel', help="""
+The CSV dialect used to read and write data files, per Python's csv module
+(default: "excel")
+    """.strip())
+
+    parser.add_option('--filter', '-F',dest='filter_expr', help="""
+An optional Python expression by which to filter input data, evaluated with
+each row's keys as local variables, e.g. "DayOfWeek == 'Monday'"
+    """.strip())
+
+    parser.add_option('--map', '-m', dest='map_expr', help="""
+An expression describing which keys to write to the output and, for each, an
+optional expression to evaluate. This is like a SQL SELECT clause, except with
+"=" instead of the "AS" keyword: "foo=Foo" lowercases the "Foo" column and
+excludes other columns; "*,date=DateTime[0:10]" copies all columns and creates
+a new "date" column containing the first 10 chars of the "DateTime" column.
+    """.strip())
+
+    parser.add_option('--empty', '-e',dest='allow_empty', action='store_true', help="""
+By default we discard rows for which the key expression evaluates to an empty value.
+Setting this flag forces the inclusion of empty keys, which will likely produce
+unusual filenames (".csv").
+    """.strip())
+
     options, args = parser.parse_args()
 
     if not options.key and not options.key_expr:
@@ -93,7 +144,7 @@ if __name__ == '__main__':
         'key':                  options.key,
         'key_expr':             options.key_expr,
         'dialect':              options.dialect,
-        'filename_template':    options.filename_template,
+        'filename':             options.filename,
         'filter_expr':          options.filter_expr,
         'map_expr':             options.map_expr,
         'out_dialect':          options.dialect,
